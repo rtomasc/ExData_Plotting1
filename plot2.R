16a70307fa7b935c846662b08911e294a2d5bc03
Rosa Tomas <rtomasc@users.noreply.github.com>
2016-01-10 21:14:50 +0100

    Create plot2.R

---
+++ plot2.R
@@ -0,0 +1,32 @@
+#PART 0. Downloading the zip file and getting data. Already done. 
+
+#fileUrl<-"https://archive.ics.uci.edu/ml/machine-learning-databases/00235/household_power_consumption.zip"
+#download.file(FileUrl, destfile="household_power_consumption.zip")
+#downloaded<-date()
+# Look at the file name in Houshold.zip. Get the file names to extract them. 
+#unzip("household_power_consumption.zip", files="household_power_consumption/household_power_consumption.txt")
+#Loading libraries
+library(data.table)
+library(dplyr)
+#Reading the whole txt (csv)
+HouseholdTotalData <- read.csv("household_power_consumption/household_power_consumption.txt", header=TRUE, sep=";", na.strings = "?")
+#Convert Date and Time to character and paste
+HouseholdTotalData$DateC<- as.character(HouseholdTotalData$Date)
+HouseholdTotalData$TimeC<- as.character(HouseholdTotalData$Time)
+#Extracting two days
+HouseholdData1<-HouseholdTotalData[HouseholdTotalData$Date=="1/2/2007",]
+HouseholdData2<-HouseholdTotalData[HouseholdTotalData$Date=="2/2/2007",]
+HouseholdData<-rbind(HouseholdData1, HouseholdData2)
+#adding Date in POSIXlt --> NewDate
+HouseholdData$DateTimeC<- paste(HouseholdData$DateC, HouseholdData$TimeC, sep=" ")
+HouseholdData$NewDate=strptime(HouseholdData$DateTimeC, "%d/%m/%Y %H:%M:%S")
+#constructing weekday variable based on $NewDate
+HouseholdData$weekday<-weekdays(HouseholdData$NewDate, abbreviate=TRUE)
+
+
+#PLOT 2 
+par(mfrow = c(1,1))
+png(filename = "plot2.png",width = 480, height = 480, units = "px")
+with(HouseholdData, plot(NewDate, Global_active_power, ylab="Global Active Power (kilowatts)", xlab=" ", pch=NA_integer_))
+lines(HouseholdData$NewDate, HouseholdData$Global_active_power)
+dev.off()
