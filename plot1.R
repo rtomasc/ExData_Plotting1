b3944798d2ebd8c0e8153946edb5d7dd3e88a01a
rtomasc <rmtomas@grupsagessa.com>
2016-01-16 20:02:58 +0100

    Third commit

--- plot1.R
+++ plot1.R
@@ -1,26 +1,21 @@
-#PART 0. Downloading the zip file and getting data. Already done. 
+#PART 0. Downloading the zip file and getting data.
 
-#fileUrl<-"https://archive.ics.uci.edu/ml/machine-learning-databases/00235/household_power_consumption.zip"
-#download.file(FileUrl, destfile="household_power_consumption.zip")
+fileUrl<-"https://archive.ics.uci.edu/ml/machine-learning-databases/00235/household_power_consumption.zip"
+download.file(FileUrl, destfile="household_power_consumption.zip")
 #downloaded<-date()
-# Look at the file name in Houshold.zip. Get the file names to extract them. 
-#unzip("household_power_consumption.zip", files="household_power_consumption/household_power_consumption.txt")
+#Look at the file name in Houshold.zip. Get the file names to extract them. 
+unzip("household_power_consumption.zip", files="household_power_consumption.txt")
 #Loading libraries
 library(data.table)
 library(dplyr)
-#Reading the whole txt (csv)
-HouseholdTotalData <- read.csv("household_power_consumption/household_power_consumption.txt", header=TRUE, sep=";", na.strings = "?")
-#Convert Date and Time to character and paste
-HouseholdTotalData$DateC<- as.character(HouseholdTotalData$Date)
-HouseholdTotalData$TimeC<- as.character(HouseholdTotalData$Time)
-#Extracting two days
-HouseholdData1<-HouseholdTotalData[HouseholdTotalData$Date=="1/2/2007",]
-HouseholdData2<-HouseholdTotalData[HouseholdTotalData$Date=="2/2/2007",]
-HouseholdData<-rbind(HouseholdData1, HouseholdData2)
+#Reading the whole txt (it's in csv format)
+HouseholdTotalData <- read.csv("household_power_consumption/household_power_consumption.txt", header=TRUE, sep=";", na.strings = "?", stringsAsFactors = FALSE)
+#Extracting two days to HouseholdData
+HouseholdData<-HouseholdTotalData[HouseholdTotalData$Date=="1/2/2007" | HouseholdTotalData$Date=="2/2/2007", ]
 #adding Date in POSIXlt --> NewDate
-HouseholdData$DateTimeC<- paste(HouseholdData$DateC, HouseholdData$TimeC, sep=" ")
-HouseholdData$NewDate=strptime(HouseholdData$DateTimeC, "%d/%m/%Y %H:%M:%S")
-#constructing weekday variable based on $NewDate
+#HouseholdData$DateTimeC<- paste(HouseholdData$Date, HouseholdData$Time, sep=" ")
+HouseholdData$NewDate=strptime(paste(HouseholdData$Date, HouseholdData$Time, sep=" "), "%d/%m/%Y %H:%M:%S")
+#adding weekday
 HouseholdData$weekday<-weekdays(HouseholdData$NewDate, abbreviate=TRUE)
 
 
